e1db321ddb32747e5917679ccea802b6da0c799b
李世达 <lisd@6renyou.com>
2018-12-28 11:20:36 +0800

    first commit

---
+++ LPerson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LPerson
+//
+//  Created by 李世达 on 2018/12/28.
+//  Copyright © 2018 李世达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
